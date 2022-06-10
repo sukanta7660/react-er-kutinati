32f6c8a941c21ce14ebb43e723358f2f83dc5438
Sukanta Purkayastha <30552975+sukanta7660@users.noreply.github.com>
2022-06-10 16:21:44 +0600

    create middleware

--- Redux-demo/index.js
+++ Redux-demo/index.js
@@ -1,6 +1,11 @@
 const redux = require('redux')
+const reduxLogger = require('redux-logger')
+
+
 const createStore = redux.createStore
 const combineReducer = redux.combineReducers
+const applyMiddleware = redux.applyMiddleware
+const logger = reduxLogger.createLogger()
 
 const BUY_CAKE = 'BUY_CAKE'
 const BUY_ICECREAM = 'BUY_ICECREAM'
@@ -74,11 +79,11 @@ const rootReducer = combineReducer({
   iceCream: iceCreamReducer
 })
 
-const store = createStore(rootReducer)
+const store = createStore(rootReducer, applyMiddleware(logger))
 
 console.log('Initial state', store.getState())
 
-const unsubscribe = store.subscribe(() => console.log('Updated State', store.getState()))
+const unsubscribe = store.subscribe(() => {})
 
 store.dispatch(buyCake())
 store.dispatch(buyCake())
